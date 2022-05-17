afea10160f4479a2f32b114971947bee6551937f
gabrielasigolo <gabrielasigolo12@gmail.com>
2022-05-17 16:02:28 -0300

    first commit

---
+++ src/app/estudantes/estudantes.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-estudantes',
+  templateUrl: './estudantes.component.html',
+  styleUrls: ['./estudantes.component.css']
+})
+export class EstudantesComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
