66b7dcc9ba1864684cd3eed57366ffc5aafa1053
gabrielasigolo <gabrielasigolo12@gmail.com>
2022-06-07 16:23:06 -0300

    adicionado HTTPS

--- src/app/estudante.service.ts
+++ src/app/estudante.service.ts
@@ -3,13 +3,21 @@ import { Estudante } from './estudante';
 import { ESTUDANTES } from './mock-students';
 import { Observable, of } from 'rxjs';
 import { MessageService } from './message.service';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
 })
 export class EstudanteService {
 
-  constructor(private messageService: MessageService) { }
+  constructor(private http: HttpClient, private messageService: MessageService) { }
+
+  // private heroesUrl = ;
+
+  /** Log a HeroService message with the MessageService */
+    private log(message: string) {
+      this.messageService.add(`StudentService: ${message}`);
+    }
 
   getEstudantes(): Observable<Estudante[]> {
     const estudantes = of(ESTUDANTES);
